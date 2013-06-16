3b0756414ceed450191dc24581be78e3d03aad44
Radu Vlad <radu.a.vlad@gmail.com>
2013-06-16 01:42:17 -0700

    trying a simpler configuration

--- lib/capistrano-recipes/test.rb
+++ lib/capistrano-recipes/test.rb
@@ -1,24 +1,32 @@
 require 'capistrano'
 
-module CapistranoRecipes
-  class Test
-    TASKS = [
-      'radu:awesomeness'
-    ]
-
-    def self.load_into(capistrano_config)
-      capistrano_config.load do
-        namespace :radu do
-          desc "Awesomeness"
-          task :awesomeness do
-            puts "yoyo wat up"
-          end
-        end
-      end
+Capistrano::Configuration.instance(:must_exist).load do
+  namespace :radu do
+    desc "Awesomeness"
+    task :more_awesomeness do
+      puts "yoyo wat up"
     end
   end
 end
-
-if Capistrano::Configuration.instance
-  CapistranoRecipes::Test.load_into(Capistrano::Configuration.instance)
-end
+#module CapistranoRecipes
+#  class Test
+#    TASKS = [
+#      'radu:awesomeness'
+#    ]
+#
+#    def self.load_into(capistrano_config)
+#      capistrano_config.load do
+#        namespace :radu do
+#          desc "Awesomeness"
+#          task :awesomeness do
+#            puts "yoyo wat up"
+#          end
+#        end
+#      end
+#    end
+#  end
+#end
+#
+#if Capistrano::Configuration.instance
+#  CapistranoRecipes::Test.load_into(Capistrano::Configuration.instance)
+#end
