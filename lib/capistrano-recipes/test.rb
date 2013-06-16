3b0c474331c9b6207c1038b5fb7bfc76b0100749
Radu Vlad <radu.a.vlad@gmail.com>
2013-06-16 01:57:26 -0700

    adding my recipes

--- lib/capistrano-recipes/test.rb
+++ lib/capistrano-recipes/test.rb
@@ -1,5 +1,3 @@
-require 'capistrano'
-
 Capistrano::Configuration.instance(:must_exist).load do
   namespace :radu do
     desc "Awesomeness"
@@ -8,25 +6,3 @@ Capistrano::Configuration.instance(:must_exist).load do
     end
   end
 end
-#module CapistranoRecipes
-#  class Test
-#    TASKS = [
-#      'radu:awesomeness'
-#    ]
-#
-#    def self.load_into(capistrano_config)
-#      capistrano_config.load do
-#        namespace :radu do
-#          desc "Awesomeness"
-#          task :awesomeness do
-#            puts "yoyo wat up"
-#          end
-#        end
-#      end
-#    end
-#  end
-#end
-#
-#if Capistrano::Configuration.instance
-#  CapistranoRecipes::Test.load_into(Capistrano::Configuration.instance)
-#end
