77992bde481bdaf2f6964fe289fbe59ca0fd9193
Radu Vlad <radu.a.vlad@gmail.com>
2013-06-16 02:02:55 -0700

    loading recipes

--- lib/capistrano-recipes.rb
+++ lib/capistrano-recipes.rb
@@ -1,3 +1,5 @@
 require "capistrano-recipes/version"
-require "capistrano-recipes/test"
+require "capistrano"
+
+Dir.glob(File.join(File.dirname(__FILE__), '/capistrano-recipes/*.rb')).sort.each { |f| load f }
 
