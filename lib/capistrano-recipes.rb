d9e022cbd8e03e2c29253edde4995b806af227b1
Radu Vlad <radu.a.vlad@gmail.com>
2013-06-16 19:16:22 -0700

    check that cap config instance exists

--- lib/capistrano-recipes.rb
+++ lib/capistrano-recipes.rb
@@ -4,27 +4,28 @@ require "capistrano-recipes/version"
 
 require 'capistrano'
 
-Capistrano::Configuration.instance.load do
-  Dir.glob(File.join(File.dirname(__FILE__), '/capistrano-recipes/*.rb')).sort.each { |f| load f }
+if Capistrano::Configuration.instance
+  Capistrano::Configuration.instance.load do
+    Dir.glob(File.join(File.dirname(__FILE__), '/capistrano-recipes/*.rb')).sort.each { |f| load f }
 
-  server "23.21.115.207", :app, :web, :db, :primary => true
-  #set :nginx_server_name, ".dreamcodelabs.com dreamcodelabs.raduvlad.com"
-  #set :application, "dreamcode"
+    server "23.21.115.207", :app, :web, :db, :primary => true
+    #set :nginx_server_name, ".dreamcodelabs.com dreamcodelabs.raduvlad.com"
+    #set :application, "dreamcode"
 
-  set :user, "deployer"
-  set :deploy_to, "/home/#{user}/apps/#{application}"
-  set :deploy_via, :remote_cache
-  set :use_sudo, false
+    set :user, "deployer"
+    set :deploy_to, "/home/#{user}/apps/#{application}"
+    set :deploy_via, :remote_cache
+    set :use_sudo, false
 
-  set :scm, :git
-  set :repository, "git@git.dreamcodelabs.com:radu/#{application}.git"
-  set :branch, "master"
-  set :rails_env, "production"
+    set :scm, :git
+    set :repository, "git@git.dreamcodelabs.com:radu/#{application}.git"
+    set :branch, "master"
+    set :rails_env, "production"
 
-  default_run_options[:pty] = true # for password prompts
-  ssh_options[:forward_agent] = true # forward local ssh keys to server (make sure you've used ssh-add for each key you want to forward)
+    default_run_options[:pty] = true # for password prompts
+    ssh_options[:forward_agent] = true # forward local ssh keys to server (make sure you've used ssh-add for each key you want to forward)
 
-  after "deploy", "deploy:cleanup" # keep only the last 5 releases
+    after "deploy", "deploy:cleanup" # keep only the last 5 releases
+  end
 end
-
 
