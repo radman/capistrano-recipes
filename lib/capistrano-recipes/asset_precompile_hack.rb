e6cae5b5658f715c47365892ac8ec3d1c361cab4
Radu Vlad <radu@dreamcodelabs.com>
2014-01-24 16:27:44 -0800

    Update asset_precompile_hack.rb

--- lib/capistrano-recipes/asset_precompile_hack.rb
+++ lib/capistrano-recipes/asset_precompile_hack.rb
@@ -2,19 +2,13 @@ namespace :deploy do
     namespace :assets do
       task :precompile, :roles => :web, :except => { :no_release => true } do
         
-        assets_changed = false
-        if releases.length < 1
-          assets_changed = true
-        else
-          from = source.next_revision(current_revision)
-          assets_changed = capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
-        end
+        #from = source.next_revision(current_revision)
         
-        if assets_changed
+        #if releases.length <= 1 || capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
           run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
-        else
-          logger.info "Skipping asset pre-compilation because there were no asset changes"
-        end
+        #else
+          #logger.info "Skipping asset pre-compilation because there were no asset changes"
+        #end
         
     end
   end
