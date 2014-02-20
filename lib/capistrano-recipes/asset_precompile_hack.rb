deed339528b286bec903f2bf296b69b6d1327079
Radu Vlad <radu@dreamcodelabs.com>
2014-02-20 13:06:29 -0800

    fixing asset precompile hack

--- lib/capistrano-recipes/asset_precompile_hack.rb
+++ lib/capistrano-recipes/asset_precompile_hack.rb
@@ -1,14 +1,21 @@
 namespace :deploy do
-    namespace :assets do
-      task :precompile, :roles => :web, :except => { :no_release => true } do
-        # NOTE: I sometimes run into problems with this on first releases, i.e. on cap deploy:cold with no symlink pointing to current release
+  namespace :assets do
+    task :precompile, :roles => :web, :except => { :no_release => true } do
+      if releases.length < 1 #<=1
+        logger.info "Not skipping asset precompilation because this is the first release."
+        do_precompile and return
+      else
         from = source.next_revision(current_revision)
-        
-        if releases.length <= 1 || capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
-          run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
-        else
-          logger.info "Skipping asset pre-compilation because there were no asset changes"
+        if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+          do_precompile and return
         end
+      end
+
+      logger.info "Skipping asset pre-compilation because there were no asset changes"
+    end
+
+    def do_precompile
+      run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
     end
   end
 end
