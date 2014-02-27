003be5fd8dbb0e4855151d983990e427f3aa13c2
Radu Vlad <radu@dreamcodelabs.com>
2014-02-26 19:00:16 -0800

    testing config vars

--- lib/capistrano-recipes.rb
+++ lib/capistrano-recipes.rb
@@ -6,6 +6,8 @@ require 'capistrano'
 
 if Capistrano::Configuration.instance
   Capistrano::Configuration.instance.load do
+    set :config_files, %w[radu fog aws] # used by config.rb
+
     Dir.glob(File.join(File.dirname(__FILE__), '/capistrano-recipes/*.rb')).sort.each { |f| load f }
 
     server "23.21.115.207", :app, :web, :db, :primary => true
---
+++ lib/capistrano-recipes/config.rb
@@ -0,0 +1,21 @@
+namespace :config do
+  config_files.each do |config_file|
+    namespace "#{config_file}" do
+
+      desc "Generate #{config_file}.yml"
+      task :setup, :roles => :app do
+        run "mkdir -p #{shared_path}/config"
+        template "#{config_file}.yml.erb", "#{shared_path}/config/#{config_file}.yml"
+      end
+      after "deploy:setup", "config:#{config_file}:setup"
+
+      desc "Symlink #{config_file}.yml into latest release"
+      task :symlink, roles: :app do
+        run "ln -nfs #{shared_path}/config/#{config_file}.yml #{release_path}/config/#{config_file}.yml"
+      end
+      after "deploy:finalize_update", "config:#{config_file}:symlink"
+
+    end
+  end
+end
+
