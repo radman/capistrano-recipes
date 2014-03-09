57b1e22c6fbe16e30bc5a16151239cd584f3248b
Radu Vlad <radu@dreamcodelabs.com>
2014-03-08 16:02:26 -0800

    adding force assets capability

--- lib/capistrano-recipes/asset_precompile_hack.rb
+++ lib/capistrano-recipes/asset_precompile_hack.rb
@@ -1,13 +1,18 @@
 namespace :deploy do
   namespace :assets do
     task :precompile, :roles => :web, :except => { :no_release => true } do
-      if releases.length <= 1
-        logger.info "Not skipping asset precompilation because this is the first release."
+      if ENV['force_assets'] == '1'
+        logger.info "Forcing asset precompilation."
         do_precompile and return
       else
-        from = source.next_revision(current_revision)
-        if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+        if releases.length <= 1
+          logger.info "Not skipping asset precompilation because this is the first release."
           do_precompile and return
+        else
+          from = source.next_revision(current_revision)
+          if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+            do_precompile and return
+          end
         end
       end
 
